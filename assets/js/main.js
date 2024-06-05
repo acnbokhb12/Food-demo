b6ee078ef39846dde353abf61f5cab43273fabfc
unknown <acnbokhb12@example.com>
2024-06-05 18:05:28 +0700

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,25 @@ document.addEventListener('click', (event) => {
     if (event.target !== tick && event.target !== searchBtn) {
         // textInput.style.display = 'none';
       tick.classList.remove('open');
-
+      
     }
 });
- 
 
+const inforUser = document.querySelector('.header__top-infor-intro');
+const detailListUser = document.querySelector('.header__top-infor-welcome-about-acc');
+inforUser.addEventListener('click',function(){
+    detailListUser.classList.add('open__header__top-infor-welcome-about-acc');
+})
+inforUser.addEventListener('click',function(event){
+    event.stopPropagation();
+})
+document.addEventListener('click', (event) => {
+    if (event.target !== inforUser && event.target !== detailListUser) {
+         
+        detailListUser.classList.remove('open__header__top-infor-welcome-about-acc');
+      
+    }
+});
+detailListUser.addEventListener('click',function(event){
+    event.stopPropagation();
+})
