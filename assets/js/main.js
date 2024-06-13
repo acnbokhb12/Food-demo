6f3d54639ef5b9383154f91d17bc9a490ef41ec5
unknown <acnbokhb12@example.com>
2024-06-13 18:08:34 +0700

    add checkout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,3 @@
-const searchBtn = document.querySelector('.js-search-icon');
-const tick = document.querySelector('.search-input');
-function showSearch() {
-    tick.classList.add('open');
-}
-searchBtn.addEventListener('click', showSearch);
-document.addEventListener('click', (event) => {
-    if (event.target !== tick && event.target !== searchBtn) {
-        // textInput.style.display = 'none';
-        tick.classList.remove('open');
-
-    }
-});
-
-const inforUser = document.querySelector('.header__top-infor-intro');
-const detailListUser = document.querySelector('.header__top-infor-welcome-about-acc');
-inforUser.addEventListener('click', function () {
-    detailListUser.classList.add('open__header__top-infor-welcome-about-acc');
-})
-inforUser.addEventListener('click', function (event) {
-    event.stopPropagation();
-})
-document.addEventListener('click', (event) => {
-    if (event.target !== inforUser && event.target !== detailListUser) {
-
-        detailListUser.classList.remove('open__header__top-infor-welcome-about-acc');
-
-    }
-});
-detailListUser.addEventListener('click', function (event) {
-    event.stopPropagation();
-})
-
 
 const btnHome = document.querySelector('.btn-list-home');
 const spanbtnHome = document.querySelector('.hambugu-navbar-icon');
