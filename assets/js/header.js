3e859fb62fc199558c8df6087c64390174925678
unknown <acnbokhb12@example.com>
2024-06-27 17:25:08 +0700

    add weekly

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    const headerHTML =`<header class="header ">
+    const headerHTML = `<header class="header ">
         <!-- header-top -->
         <section class="header__top__nav  ">
             <div class="header__top-container container">
@@ -114,7 +114,7 @@ document.addEventListener("DOMContentLoaded", function() {
                                 <li><a href="Home.html">Home</a></li>
                                 <li><a href="#">About</a></li>
                                 <li><a href="Menu.html">Menu</a></li>
-                                <li><a href="#">Blog</a></li>
+                                <li><a href="weekly.html">Weekly</a></li>
                                 <li><a href="#">Contact</a></li>
 
                             </ul>
@@ -147,68 +147,68 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // find food
     const searchBtn = document.querySelector('.js-search-icon');
-    const tick =document.querySelector('.search-input');
-    function showSearch(){
+    const tick = document.querySelector('.search-input');
+    function showSearch() {
         tick.classList.add('open');
     }
-searchBtn.addEventListener('click', showSearch);
-document.addEventListener('click', (event) => {
-    if (event.target !== tick && event.target !== searchBtn) {
-        // textInput.style.display = 'none';
-      tick.classList.remove('open');
-      
-    }
-});
+    searchBtn.addEventListener('click', showSearch);
+    document.addEventListener('click', (event) => {
+        if (event.target !== tick && event.target !== searchBtn) {
+            // textInput.style.display = 'none';
+            tick.classList.remove('open');
 
-// user accout
-const inforUser = document.querySelector('.header__top-infor-intro');
-const detailListUser = document.querySelector('.header__top-infor-welcome-about-acc');
-inforUser.addEventListener('click',function(){
-    detailListUser.classList.add('open__header__top-infor-welcome-about-acc');
-})
-inforUser.addEventListener('click',function(event){
-    event.stopPropagation();
-})
-document.addEventListener('click', (event) => {
-    if (event.target !== inforUser && event.target !== detailListUser) {
-         
-        detailListUser.classList.remove('open__header__top-infor-welcome-about-acc');
-      
-    }
-});
-detailListUser.addEventListener('click',function(event){
-    event.stopPropagation();
-})
+        }
+    });
 
-//  open navbar
-const btnHome = document.querySelector('.btn-list-home');
-const spanbtnHome = document.querySelector('.hambugu-navbar-icon');
-const listHome = document.querySelector('.nav-bar-menu');
-const btncloseNav = document.querySelector('.close-navbar-icon-btn');
+    // user accout
+    const inforUser = document.querySelector('.header__top-infor-intro');
+    const detailListUser = document.querySelector('.header__top-infor-welcome-about-acc');
+    inforUser.addEventListener('click', function () {
+        detailListUser.classList.add('open__header__top-infor-welcome-about-acc');
+    })
+    inforUser.addEventListener('click', function (event) {
+        event.stopPropagation();
+    })
+    document.addEventListener('click', (event) => {
+        if (event.target !== inforUser && event.target !== detailListUser) {
 
-btnHome.addEventListener('click', function(event) {
-    spanbtnHome.classList.add('active-btn-navbar-off');
-    listHome.classList.add('active-btn-navbar-on');
-    event.stopPropagation(); // Ngăn sự kiện click lan ra ngoài
-});
+            detailListUser.classList.remove('open__header__top-infor-welcome-about-acc');
 
-btncloseNav.addEventListener('click', function(event) {
-    listHome.classList.remove('active-btn-navbar-on');
-    spanbtnHome.classList.remove('active-btn-navbar-off');
-    event.stopPropagation(); // Ngăn sự kiện click lan ra ngoài
-});
+        }
+    });
+    detailListUser.addEventListener('click', function (event) {
+        event.stopPropagation();
+    })
 
-document.addEventListener('click', function(event) {
-    if ((!listHome.contains(event.target) && event.target !== btnHome) || event.target === btncloseNav) {
+    //  open navbar
+    const btnHome = document.querySelector('.btn-list-home');
+    const spanbtnHome = document.querySelector('.hambugu-navbar-icon');
+    const listHome = document.querySelector('.nav-bar-menu');
+    const btncloseNav = document.querySelector('.close-navbar-icon-btn');
+
+    btnHome.addEventListener('click', function (event) {
+        spanbtnHome.classList.add('active-btn-navbar-off');
+        listHome.classList.add('active-btn-navbar-on');
+        event.stopPropagation(); // Ngăn sự kiện click lan ra ngoài
+    });
+
+    btncloseNav.addEventListener('click', function (event) {
         listHome.classList.remove('active-btn-navbar-on');
         spanbtnHome.classList.remove('active-btn-navbar-off');
-    }
-});
+        event.stopPropagation(); // Ngăn sự kiện click lan ra ngoài
+    });
 
-// Ngăn sự kiện click từ bên trong menu làm ẩn menu
-listHome.addEventListener('click', function(event) {
-    event.stopPropagation();
-});
+    document.addEventListener('click', function (event) {
+        if ((!listHome.contains(event.target) && event.target !== btnHome) || event.target === btncloseNav) {
+            listHome.classList.remove('active-btn-navbar-on');
+            spanbtnHome.classList.remove('active-btn-navbar-off');
+        }
+    });
+
+    // Ngăn sự kiện click từ bên trong menu làm ẩn menu
+    listHome.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
 
 
 
